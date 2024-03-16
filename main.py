44817ced925dbcb750bfc0c53305a7e45274946c
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-03-16 20:38:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,54 +7,48 @@ import openai
 # Initialize OpenAI
 # openai.api_key = os.getenv('OPENAI_API_KEY')
 
-def tutor():
-	st.title("ChatGPT-like clone with Prompt Engineering")
 
-	openai.api_key = st.secrets["openapi_key"]
+st.title("ChatGPT-like clone with Prompt Engineering")
 
-	prompt_template = """
+openai.api_key = st.secrets["openapi_key"]
+
+prompt_template = """
 	"Speak like Einstein, a Physics teacher for every question that was asked, 
 	explain as clearly as possible, assuming the students know very little prior knowledge"
 	"""
 
-	if "openai_model" not in st.session_state:
-		st.session_state["openai_model"] = "gpt-3.5-turbo"
+if "openai_model" not in st.session_state:
+	st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-	if "msg_bot" not in st.session_state:
-		st.session_state.msg_bot = []
+if "msg_bot" not in st.session_state:
+	st.session_state.msg_bot = []
 
-	for message in st.session_state.msg_bot:
-		with st.chat_message(message["role"]):
-			st.markdown(message["content"])
+for message in st.session_state.msg_bot:
+	with st.chat_message(message["role"]):
+	st.markdown(message["content"])
 
-	try:
+try:
 
-		if prompt := st.chat_input("What is up?"):
-			st.session_state.msg_bot.append({"role": "user", "content": prompt})
-			with st.chat_message("user"):
-				st.markdown(prompt)
+	if prompt := st.chat_input("What is up?"):
+		st.session_state.msg_bot.append({"role": "user", "content": prompt})
+		with st.chat_message("user"):
+			st.markdown(prompt)
 
-			with st.chat_message("assistant"):
-				message_placeholder = st.empty()
-				full_response = ""
-				for response in openai.ChatCompletion.create(
-					model=st.session_state["openai_model"],
-					messages=[
-								{"role": "system", "content": prompt_template},
-								{"role": "user", "content": prompt},
-							],
-					stream=True,
-				):
-					full_response += response.choices[0].delta.get("content", "")
-					message_placeholder.markdown(full_response + "▌")
-				message_placeholder.markdown(full_response)
-			st.session_state.msg_bot.append({"role": "assistant", "content": full_response})
+		with st.chat_message("assistant"):
+			message_placeholder = st.empty()
+			full_response = ""
+			for response in openai.ChatCompletion.create(
+				model=st.session_state["openai_model"],
+				messages=[
+							{"role": "system", "content": prompt_template},
+							{"role": "user", "content": prompt},
+						],
+				stream=True,
+			):
+				full_response += response.choices[0].delta.get("content", "")
+				message_placeholder.markdown(full_response + "▌")
+			message_placeholder.markdown(full_response)
+		st.session_state.msg_bot.append({"role": "assistant", "content": full_response})
 
-	except Exception as e:
-		st.error(e)
-   
-def main():
-    tutor()
-
-if __name__ == "__main__":
-    main()
+except Exception as e:
+	st.error(e)
