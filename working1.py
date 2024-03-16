bcabce77e5a038157bc76c1d11811e94a9c1eeaa
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-03-16 22:21:24 +0800

    Create working1.py

---
+++ working1.py
@@ -0,0 +1,53 @@
+import os
+import streamlit as st
+from openai import OpenAI
+# from openai import OpenAI
+client = OpenAI(
+	api_key=os.environ['OPENAI_API_KEY'], 
+)
+
+# Initialize OpenAI
+# openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+st.title("Physics Tutor")
+
+#Change here onwards
+
+
+# Set a default model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+	    {"role": "system", "content": "Speak like a middle school Physics teacher for every question that was asked. Explain as clearly as possible, assuming the students know very little prior knowledge."},
+    ]
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+	    
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    
+    st.session_state.messages.append({"role": "assistant", "content": response})
+	
