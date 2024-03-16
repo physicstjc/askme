f46dc9f63478c332c7d2d9ea504aa9a47a9fb9c5
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-03-16 21:35:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,16 +11,25 @@ client = OpenAI(
 
 
 st.title("Physics Tutor")
+
 myinput = st.chat_input("What is up?")
 
-response = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "Speak like a middle school Physics teacher for every question that was asked. Explain as clearly as possible, assuming the students know very little prior knowledge."},
-    {"role": "user", "content": myinput}
-  ],
-  stream=True,
-)
+try:
+	if myinput := st.chat_input("What is up?"):
+		st.session_state.msg_bot.append({"role": "user", "content": myinput})
+		with st.chat_message("user"):
+			st.markdown(myinput)
+
+		with st.chat_message("assistant"):
+			message_placeholder = st.empty()
+			response = client.chat.completions.create(
+  				model="gpt-3.5-turbo",
+  				messages=[
+					{"role": "system", "content": "Speak like a middle school Physics teacher for every question that was asked. Explain as clearly as possible, assuming the students know very little prior knowledge."},
+					{"role": "user", "content": myinput},
+				],
+				stream=True,
+			)
 
 st.markdown(response.choices[0].message.content)
 
