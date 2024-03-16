fe2ac7498dc6951cc4732a4a1c9acea8b331bf35
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-03-16 20:23:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,40 +10,58 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 st.title('OpenAI Physics Tutoring Assistant Demo')
 user_input = st.text_input("Enter your text here")
 
-assistant = client.beta.assistants.create(
-  name="Math Tutor",
-  instructions="You are a personal math tutor. Write and run code to answer math questions.",
-  tools=[{"type": "code_interpreter"}],
-  model="gpt-4-turbo-preview",
-)
+def tutor():
+    #Physics Tutor
+    st.title("Ask your question about https://iwant2study.org/ospsg/")
 
-thread = client.beta.threads.create()
+    openai.api_key = st.secrets["openapi_key"]
 
-message = client.beta.threads.messages.create(
-    thread_id=thread.id,
-    role="user",
-    content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
-)
+    prompt_template = """
+    "Speak like a learning by doing physics teacher who creates hundreds of Easy JavaScript Simulations and uses the video analysis and modeling tool Tracker for educational question that is asked. Answer in Singaporean style, called Singlish.
+    Explain as clearly as possible in at most 2 paragraphs, elaborate in no more than 100 words, assuming the students know very little prior knowledge. Make reference to actual working URLs that work to interactive resources found at https://iwant2study.org/ospsg/index.php/sitemap to help students make sense of Physics.
+    Your tone should be polite and words chosen should be simple.
+You are committed to providing a respectful and inclusive environment and will not tolerate
+racist, discriminatory, or offensive language. You must not respond to politically sensitive
+matters that concern national security, particularly within Singapore's context. If you don't
+know or are unsure of any information, just say you do not know. Do not make up information."
+    """
 
-run = client.beta.threads.runs.create(
-  thread_id=thread.id,
-  assistant_id=assistant.id,
-  instructions="Please address the user as Jane Doe. The user has a premium account."
-)
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
 
-import time
-  
-while run.status in ['queued', 'in_progress', 'cancelling']:
-  time.sleep(1) # Wait for 1 second
-  run = client.beta.threads.runs.retrieve(
-    thread_id=thread.id,
-    run_id=run.id
-  )
+    if "msg_bot" not in st.session_state:
+        st.session_state.msg_bot = []
 
-if run.status == 'completed': 
-  messages = client.beta.threads.messages.list(
-    thread_id=thread.id
-  )
-  print(messages)
-else:
-  print(run.status)
+    for message in st.session_state.msg_bot:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    try:
+
+        if prompt := st.chat_input("What is up?"):
+            st.session_state.msg_bot.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
+
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+                full_response = ""
+                for response in openai.ChatCompletion.create(
+                    model=st.session_state["openai_model"],
+                    messages=[
+                                {"role": "system", "content": prompt_template},
+                                {"role": "user", "content": prompt},
+                            ],
+                    stream=True,
+                ):
+                    full_response += response.choices[0].delta.get("content", "")
+                    message_placeholder.markdown(full_response + "▌")
+                message_placeholder.markdown(full_response)
+            st.session_state.msg_bot.append({"role": "assistant", "content": full_response})
+
+    except Exception as e:
+        st.error(e)
+
+   
+def main():
+    tutor()
