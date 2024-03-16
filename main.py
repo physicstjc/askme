095ecc86914b3f3a70ce9176286c14484a39bf74
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-03-16 22:22:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if "openai_model" not in st.session_state:
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = [
-	    {"role": "system", "content": "Speak like a middle school Physics teacher for every question that was asked. Explain as clearly as possible, assuming the students know very little prior knowledge."},
+	    {"role": "system", "content": "Speak like a middle school Physics teacher for every question that was asked. Explain as clearly as possible, assuming the students know very little prior knowledge. If mathematical working is needed, display in Latex rendered images."},
     ]
 
 # Display chat messages from history on app rerun
