c4a7523b011a834c4d5b8f9d6bd4cb6afe4d501c
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-03-16 21:42:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,21 +14,21 @@ st.title("Physics Tutor")
 
 myinput = st.chat_input("What is up?")
 
-try:
-	if myinput := st.chat_input("What is up?"):
-		st.session_state.msg_bot.append({"role": "user", "content": myinput})
-		with st.chat_message("user"):
-			st.markdown(myinput)
+if myinput := st.chat_input("What is up?"):
+	st.session_state.msg_bot.append({"role": "user", "content": myinput})
+	with st.chat_message("user"):
+		st.markdown(myinput)
 
-		with st.chat_message("assistant"):
-			response = client.chat.completions.create(
-  				model="gpt-3.5-turbo",
-  				messages=[
-					{"role": "system", "content": "Speak like a middle school Physics teacher for every question that was asked. Explain as clearly as possible, assuming the students know very little prior knowledge."},
-					{"role": "user", "content": myinput},
-				],
-				stream=True,
-			)
+	with st.chat_message("assistant"):
+		response = client.chat.completions.create(
+  			model="gpt-3.5-turbo",
+  			messages=[
+				{"role": "system", "content": "Speak like a middle school Physics teacher for every question that was asked. Explain as clearly as possible, assuming the students know very little prior knowledge."},
+				{"role": "user", "content": myinput},
+			],
+			stream=True,
+		)
+		st.markdown(response.choices[0].message.content)
 
-st.markdown(response.choices[0].message.content)
+
 
