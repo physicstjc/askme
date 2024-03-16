a77e2d4b2710de834c69a601ee3a6d545d8d69cb
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-03-16 21:22:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ client = OpenAI(
 
 
 st.title("Physics Tutor")
-input = st.chat_input("What is up?")
+text_input = st.chat_input("What is up?")
 
 response = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
     {"role": "system", "content": "Speak like a middle school Physics teacher for every question that was asked. Explain as clearly as possible, assuming the students know very little prior knowledge."},
-    {"role": "user", "content": "Explain the {input}"}
+    {"role": "user", "content": text_input}
   ]
 )
 
