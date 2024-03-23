df8e47c94e63b9f1c93d66ccca7dcb78f23d8915
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-03-23 09:52:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ if "messages" not in st.session_state:
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    if message["role"] != "system":
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+#   if message["role"] != "system":
+    with st.chat_message(message["role"]):
+	st.markdown(message["content"])
 
 # Accept user input
 if prompt := st.chat_input("What is up?"):
