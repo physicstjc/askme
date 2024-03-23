0e347c2993c31bbb95c318e6e61daa415e018c2b
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-03-23 09:53:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ if "messages" not in st.session_state:
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-#   if message["role"] != "system":
     with st.chat_message(message["role"]):
 	st.markdown(message["content"])
 
