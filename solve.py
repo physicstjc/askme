eeac9ad78acd3e24444a5f50bf9f42f2b7f04d40
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-04-07 13:57:45 +0800

    Update solve.py

--- solve.py
+++ solve.py
@@ -13,6 +13,8 @@ import tempfile
 import shutil
 from PIL import UnidentifiedImageError
 
+# Currently not analysing the image. Image is available in S3.
+
 s3_client = boto3.client('s3', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
 
 def upload_to_s3(uploaded_file, bucket_name, object_name=None):
