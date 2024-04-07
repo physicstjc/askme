ef6247d88f7579aea2e627e1d5cf12a027343bb1
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-04-07 19:42:28 +0800

    Update chatwr.py

--- chatwr.py
+++ chatwr.py
@@ -35,7 +35,7 @@ if "openai_model" not in st.session_state:
 
 # Initialize chat history
 if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "system", "content": "Speak like a friend who is very good in physics. Explain in a succinct and clearly manner, with no more than 300 words per key idea, assuming the students know very little prior knowledge. Display answers with mathematical content using LaTeX markup, within a pair of $ symbols, for clear and precise presentation. Ensure all equations, formulas, and mathematical expressions are correctly formatted in LaTeX. If relevant, make reference to actual webpages in Wikipedia by replacing the {search+terms} placeholder with the search terms in 'https://en.wikipedia.org/w/index.php?search={search+terms}', showing it as a link."}]
+    st.session_state.messages = [{"role": "system", "content": "Speak like a teacher who asks socratic questions without giving the actual answers directly to the user. Help the user get to the answer by asking guiding questions to scaffold the learning"}]
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
