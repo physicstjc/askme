ddf60dc92c6ef29a86ca3d41ab6801e0132cb42a
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-04-20 14:10:54 +0800

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -28,24 +28,23 @@ if user_input:
         ]
     )
 
-run = client.beta.threads.runs.retrieve(
-    thread_id=thread.id,
-    assistant_id=assistant.id,
-  )
- 
-# Wait for run to complete
-while run.status != 'completed': 
-  run = client.beta.threads.runs.retrieve(
-    thread_id=thread.id,
-    run_id=run.id
-  )
-  st.write(run,status)
-  time.sleep(1)
-else:
-  st.write("Run Complete!")
+    run = client.beta.threads.runs.retrieve(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+      )
+    
+    # Check periodically whether the run is done, and update the status
+    while run.status != "completed":
+        time.sleep(5)
+        status_box.update(label=f"{run.status}...", state="running")
+        run = openai_client.beta.threads.runs.retrieve(
+            thread_id=thread.id, run_id=run.id
+        )
 
-message_response = client.beta.threads.messages.list(thread_id=thread.id)
-messages = message_response.data
-
-latest_message = message[0]
-st.write(latest_message.content[0].text.value)
+    # Once the run is complete, update the status box and show the content
+    status_box.update(label="Complete", state="complete", expanded=True)
+    messages = openai_client.beta.threads.messages.list(
+        thread_id=thread.id
+    )
+    st.markdown(messages.data[0].content[0].text.value)
+  
