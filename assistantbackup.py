fd861ed15c154ddf4527356c6e1d32fa2471afe8
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-04-20 16:49:23 +0800

    Create assistantbackup.py

---
+++ assistantbackup.py
@@ -0,0 +1,36 @@
+
+
+st.title("Practice with AI")
+st.text("Which question would you like to discuss?")
+
+user_input = st.chat_input("What is up?")
+if user_input:
+    thread = client.beta.threads.create(
+        messages = [
+            {
+                "role": "user",
+                "content": user_input,
+            }
+        ]
+    )
+
+    run = client.beta.threads.runs.retrieve(
+        thread_id=thread.id,
+        assistant_id=ASSISTANT_ID,
+      )
+    
+    # Check periodically whether the run is done, and update the status
+    while run.status != "completed":
+        time.sleep(5)
+        status_box.update(label=f"{run.status}...", state="running")
+        run = openai_client.beta.threads.runs.retrieve(
+            thread_id=thread.id, run_id=run.id
+        )
+
+    # Once the run is complete, update the status box and show the content
+    status_box.update(label="Complete", state="complete", expanded=True)
+    messages = openai_client.beta.threads.messages.list(
+        thread_id=thread.id
+    )
+    st.markdown(messages.data[0].content[0].text.value)
+  
