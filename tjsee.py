0c58698d5814f359127570fe2c6c9eb5c546e4f7
physicstjc <85329791+physicstjc@users.noreply.github.com>
2024-05-02 18:57:18 +0800

    Update tjsee.py

--- tjsee.py
+++ tjsee.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import csv
 import re
 
-assistant_id  = st.secrets["assistant_id"]
+assistant_id  = st.secrets["assistant_id2"]
 
 # Set openAi client , assistant ai and assistant ai thread
 @st.cache_resource
